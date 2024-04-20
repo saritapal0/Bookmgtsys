2ad4c9bb792538e4dc9061ededa6e7a6c0e657af
saritapal0 <159772730+saritapal0@users.noreply.github.com>
2024-04-20 13:43:43 +0530

    book

--- conrtoller/cartsControllerFolder/cartsControl.js
+++ conrtoller/cartsControllerFolder/cartsControl.js
@@ -20,37 +20,17 @@ router.post('/addcart',async(req,res)=>{
     res.status(201).send('created successfully')
 })
 
-//http://localhost:3000/api/carts/addcart/
+//http://localhost:3000/api/carts/removecart/
 
-router.post('/addcart',async(req,res)=>{
-   await service.addcart(req.body)
-  res.status(201).send('created successfully')
+router.post('/removecart',async(req,res)=>{
+   await service.removecart(req.body)
+  res.status(201).send('remove successfully')
 })
 
-//http://localhost:3000/api/carts/addcart/
+//http://localhost:3000/api/carts/emptycart/
 
-router.post('/addcart',async(req,res)=>{
-   await service.addcart(req.body)
-  res.status(201).send('created successfully')
+router.post('/emptycart',async(req,res)=>{
+   await service.emptycart(req.body)
+  res.status(201).send('cart is empty')
 })
-
-//http://localhost:3000/api/carts/updatecart/cart_id
-
-router.put('/updatecart/:cart_id',async(req,res)=>{
-   const affectedRows = await service.updatecart(req.body,req.params.cart_id)
-   if(affectedRows == 0)
-   res.status(404).json('no record id:'+req.params.id)
-   else
-   res.send('updated successful')
-
-})
-
-//http://localhost:3000/api/carts/deletecart/cart_id
-
-router.delete('/deleteclient/:client_id',async(req,res)=>{
-    const affectedRows = await service.deleteclient(req.params.client_id)
-    if(affectedRows == 0)
-    res.status(404).json('no record id:'+req.params.id)
-    res.send('delete successful')
-   })
 module.exports = router;
--- services/carts/carts_sevices.js
+++ services/carts/carts_sevices.js
@@ -1,11 +1,6 @@
 const db = require('../../db')
 
 
-module.exports.getAllcarts = async()=> {
-    const [rows] = await db.query('SELECT * FROM carts')
-return rows;
-}
-
 module.exports.getcartById=async(id)=> {
     const [rows] = await db.query('SELECT * FROM carts WHERE cart_id=?',[id])
     return rows;
@@ -15,15 +10,16 @@ module.exports.addcart = async(newCart)=>{
     const [rows]=await db.query('INSERT INTO carts SET ?',newCart)
     return rows;
 }
-
-module.exports.updatecart = async ( cartData, cart_id) => {
-    const query = "UPDATE carts SET ? WHERE cart_id = ?";
-    const [{affectedRows}] = await db.query(query, [cartData, cart_id]);
-    return affectedRows;
-};
-
-module.exports.deletecart=async(cart_id)=>{
-    const [{affectedRows}] = await db.query('DELETE FROM carts WHERE client_id=?',[cart_id])
-    return affectedRows;
+module.exports.removecart = async(removeCart)=>{
+    const [rows]=await db.query('Delete INTO carts SET ?',removeCart)
+    return rows;
 }
+
+module.exports.emptycart = async(newCart)=>{
+    const [rows]=await db.query('INSERT INTO carts SET ?',newCart)
+    return rows;
+}
+
+
+
 
