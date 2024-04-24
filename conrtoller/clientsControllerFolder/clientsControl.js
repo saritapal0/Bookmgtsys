f1fec817e7615bb0e204c4b303d1d103e804dda6
saritapal0 <159772730+saritapal0@users.noreply.github.com>
2024-04-24 10:55:19 +0530

    book

--- conrtoller/clientsControllerFolder/clientsControl.js
+++ conrtoller/clientsControllerFolder/clientsControl.js
@@ -1,29 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require("jsonwebtoken");
-const secretkey = "secretkey"
 const service = require('../../services/clients/clients_services');
 const ResponseManager = require('../../response/responseManager');
 
 
 
-//jwt tokens authentication
-function varifyToken(req, res, next) {
-   const token = req.headers["authorization"];
-   console.log("middleware", token);
-   if (token) {
-     let token = token.split("")[1];
-     jwt.verify(token, jwt, (err, valid) => {
-       if (err) {
-         res.status(401).send({ result: "plz provide valid token" });
-       } else {
-         next();
-       }
-     });
-   } else {
-     res.status(403).send({ result: "plz add token with header" });
-   }
- }
+
 //Retrieve a list of all clients.
 //http://localhost:3000/api/clients/getAllclients/
 
@@ -45,7 +27,7 @@ router.get('/getclient/:client_id',async(req,res)=>{
 //Add a new client to the database.
 //http://localhost:3000/api/clients/addclient/
 
-router.post('/addclient',varifyToken,async(req,res)=>{
+router.post('/addclient',async(req,res)=>{
  
 if (!req.body.client_name) {
     return ResponseManager.statusError(502).sendError({ error: "client_name Required" });
